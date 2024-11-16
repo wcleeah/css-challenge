95c5df5952445291b89f97a6501503c636e0c50d
Charlie Lee <c050820021a@gmail.com>
2024-11-16 22:00:13 +0800

    finish day 2

--- day2/main.js
+++ day2/main.js
@@ -1,9 +1,8 @@
 function toggle() {
-    console.log("toggle");
   var l1 = document.querySelector(".line1");
   var l2 = document.querySelector(".line2");
   var l3 = document.querySelector(".line3");
-  l1.toggleAttribute("class", "line1-l-r");
-  l2.toggleAttribute("class", "line2-hidden");
-  l3.toggleAttribute("class", "line3-r-l");
+  l1.classList.toggle("move-down-and-rotate-anti-clockwise");
+  l3.classList.toggle("move-up-and-rotate-clockwise");
+  l2.classList.toggle("rotate-anti-clockwise");
 }
